e1e43035482a56e5a6a3dc993a0038238f7bdb71
Alexis Hevia <hevia.alexis@gmail.com>
2019-08-28 22:52:06 -0500

    Export dropbox action creators

--- src/dropbox/actions.js
+++ src/dropbox/actions.js
@@ -1,3 +1,15 @@
 export const LOGIN = 'reduxFileSync/dropbox/login';
 export const LOGOUT = 'reduxFileSync/dropbox/logout';
 export const SELECT_FILE = 'reduxFileSync/dropbox/selectFile';
+
+export function login(accessToken) {
+  return { type: LOGIN, payload: accessToken };
+}
+
+export function logout() {
+  return { type: LOGOUT };
+}
+
+export function selectFile(filepath) {
+  return { type: SELECT_FILE, payload: filepath };
+}
