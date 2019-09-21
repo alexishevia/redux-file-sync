22fc5d47ed57eba2d99c7aada7a4610a415ed84e
Alexis Hevia <hevia.alexis@gmail.com>
2019-09-21 13:45:33 -0500

    fix: pass along the 'reprocessAll' argument correctly

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ export default function ReduxFileSync(
   if (!Array.isArray(actionsToSync) || !actionsToSync.length) throw new Error('actionsToSync is required');
 
   const reducer = buildReducer(baseReducer, { whitelist, actionsToSync });
-  const sync = ({ store, localStorage, cloudStorage }) => runSync({ store, reducer, localStorage, cloudStorage });
+  const sync = args => runSync({ reducer, ...args });
 
   return {
     get reducer() {
